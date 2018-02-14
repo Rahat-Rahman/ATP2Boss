cdd6169767f16b0cb2bf6f4d0498a0436062c9cb
Rahat Rahman <rahatrm13@gmail.com>
2018-02-14 20:31:43 +0600

    Initialize

---
+++ ATP2RRSltn/ATP2RR1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ATP2RR1.MvcApplication" Language="C#" %>
---
+++ ATP2RRSltn/ATP2RR1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATP2RR1.Startup))]
+namespace ATP2RR1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
